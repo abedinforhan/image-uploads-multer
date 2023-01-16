442170c59e732d6f4cee5235b5df68b97c8d3f23
Mezbaul Abedin Forhan <67517709+abedinforhan@users.noreply.github.com>
2023-01-16 18:52:13 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
 const express = require('express');
-const multer = require('multer');
 const ejs = require('ejs');
-const path = require('path');
+const upload = require('./upload');
 
-// Set The Storage Engine
-const storage = multer.diskStorage({
-  destination: './public/uploads/',
-  filename: function(req, file, cb){
-    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-
-// Init Upload
-const upload = multer({
-  storage: storage,
-  limits:{fileSize: 1000000},
-  fileFilter: function(req, file, cb){
-    checkFileType(file, cb);
-  }
-}).single('myImage');
-
-// Check File Type
-function checkFileType(file, cb){
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
-
-  if(mimetype && extname){
-    return cb(null,true);
-  } else {
-    cb('Error: Images Only!');
-  }
-}
 
 // Init app
 const app = express();
